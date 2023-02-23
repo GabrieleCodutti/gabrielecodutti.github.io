06b83a85cd1f49211964ff5f816b2fe0f49206ee
Gabriele Codutti <116967837+GabrieleCodutti@users.noreply.github.com>
2023-02-23 18:12:33 +0100

    Update processa_form.php
    Signed-off-by: Gabriele Codutti <116967837+GabrieleCodutti@users.noreply.github.com>

--- processa_form.php
+++ processa_form.php
@@ -1,41 +1,33 @@
+
 <?php
 // Verifica se il form è stato inviato
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  // Recupera i dati del form
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-  // Valida i dati
-  $errors = array();
-  if (empty($name)) {
-    $errors[] = 'Il nome è obbligatorio.';
-  }
-  if (empty($email)) {
-    $errors[] = 'L\'indirizzo email è obbligatorio.';
-  } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    $errors[] = 'L\'indirizzo email non è valido.';
-  }
-  if (empty($message)) {
-    $errors[] = 'Il messaggio è obbligatorio.';
-  }
+    // Recupera i dati dal form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-  // Se ci sono errori, restituisci un messaggio di errore
-  if (!empty($errors)) {
-    $error_message = implode('<br>', $errors);
-    echo '<p style="color: red;">' . $error_message . '</p>';
-  } else {
-    // Invia l'email di notifica
-    $to = 'coduttigabriele@gmail.com';
-    $subject = 'Nuovo messaggio dal form di contatto';
-    $body = "Nome: $name\nEmail: $email\nMessaggio:\n$message";
-    $headers = "From: $email\r\n";
-    $headers .= "Reply-To: $email\r\n";
-    if (mail($to, $subject, $body, $headers)) {
-      echo '<p style="color: green;">Grazie per averci contattato. Ti risponderemo al più presto.</p>';
-    } else {
-      echo '<p style="color: red;">Errore nell\'invio del messaggio.</p>';
+    // Verifica che tutti i campi siano stati compilati
+    if(empty($name) || empty($email) || empty($message)) {
+        echo "Please fill in all fields.";
+        exit;
     }
-  }
+
+    // Costruisce l'email
+    $to = "coduttigabriele@gmail.com"; // Inserisci qui il tuo indirizzo email
+    $subject = "New message from your website";
+    $body = "Name: $name\nEmail: $email\n\n$message";
+
+    // Invia l'email
+    if(mail($to, $subject, $body)) {
+        echo "Your message has been sent.";
+    } else {
+        echo "There was an error sending your message. Please try again.";
+    }
+
+} else {
+    // Il form non è stato inviato correttamente
+    echo "There was an error processing your request.";
 }
 ?>
